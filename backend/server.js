0aec9adfda8b19dad780e3e7d25df0bf6075e31a
Danyal Mehmood <adanyal46@gamil.com>
2024-01-21 21:28:12 +0500

    Implementation: Incorporate Logging with Winston and Morgan.

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import cookieParser from "cookie-parser";
 import express from "express";
 import morgan from "morgan";
-import chalk from "chalk";
+import { morganMiddleware, systemLogs } from "./utils/Logger.js";
 
 const app = express();
 
@@ -13,6 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(cookieParser());
+app.use(morganMiddleware)
 
 app.get("/api/v1/test", (req, res) => {
   res.json({ hi: "Welcome to then invoice app" });
@@ -22,4 +23,5 @@ const PORT =  process.env.PORT || 2003;
 
 app.listen(PORT, () => {
     console.log(`Server is running at ${PORT}`);
+    systemLogs.info(`Server is running on ${process.env.NODE_ENV} mode on ${PORT}`)
 })
